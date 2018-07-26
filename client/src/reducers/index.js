cf85c71317c58372e9ed2f9bad4dd96d4330438f
Michael Judd <m.judd@queensu.ca>
2018-07-25 23:19:55 -0400

    Added bot actions in state

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import tradeReducer from './tradeReducer';
+import botReducer from './botReducer';
 import { firebaseReducer } from 'react-redux-firebase'
 
 
 
 export default combineReducers({
-    trade: tradeReducer,
+    trade: tradeReducer, // everything trade related
+    bot: botReducer,
     firebase: firebaseReducer // Add firebase to reducers
 })
